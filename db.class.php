06dc85e0f745dd0fdb26ddbbbcd3b00aab953c1e
cy <cy@cydeMacBook-Pro.local>
2016-07-27 23:48:30 +0800

    demo

--- db.class.php
+++ db.class.php
@@ -4,11 +4,17 @@ class DB {
 
 	private  $db = "";
 
-	public function __construct($conf)
-	{
+	public function __construct($conf){
 		$this->db = DBCONN::getDB($conf);
+		if(empty($this->db)) mysql_select_db($conf['db'],$this->db);
+
 	}
 
+
+	public public function query($sql){
+		$result = mysql_query();
+		return $result;
+	}
 
 
 
--- demo.php
+++ demo.php
@@ -2,8 +2,11 @@
 
 include_once "dbconn.class.php";
 include_once "db.class.php";
-$conf = ['host'=>'localhost','name'=>'root','password'=>'zhuzhu'];
+$conf = ['host'=>'localhost','name'=>'root','password'=>'zhuzhu','db'=>'3dgif'];
 $db = new DB($conf);
-var_dump($db);
+$sql = "select * from gifs";
+$res = $db->query($sql);
+
+var_dump($res);
 
 ?>
