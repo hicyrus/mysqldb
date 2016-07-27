507d1a2336ab21071061129afb4b431188b851f1
cy <cy@cydeMacBook-Pro.local>
2016-07-27 23:39:35 +0800

    demo

--- dbconn.class.php
+++ dbconn.class.php
@@ -5,11 +5,11 @@ final class DBCONN {
 	public static $db = "";
 	public static function getDb($conf = null){
 		if(empty($conf)) return null;
-		if(empty(DB::$db)){
-			DB::$db = mysql_connect($conf['host'],$conf['name'],$conf['password']);
-			return DB::$db;
+		if(empty(self::$db)){
+			self::$db = mysql_connect($conf['host'],$conf['name'],$conf['password']);
+			return self::$db;
 		}
-		return DB::$db ;
+		return self::$db ;
 	}
 
 	
